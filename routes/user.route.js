581f0af1859c8475b864d1d5c44796c51885ebe3
jafman <kamsy74@gmail.com>
2022-03-12 16:22:51 +0100

    implements route and controller

---
+++ routes/user.route.js
@@ -0,0 +1,13 @@
+module.exports = (app) => {
+  const userController = require("../controllers/user.controller");
+  let router = require("express").Router();
+  // Create a new User
+  router.post("/", userController.create);
+  // Get a User by email
+  router.get("/:email", userController.findOne);
+  // Update a User by id
+  router.put("/:id", userController.update);
+  // Delete a User by id
+  router.delete("/:id", userController.delete);
+  app.use("/api/user", router);
+};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 //app.set('port', port);
 
 const server = http.createServer(app);
+require("./routes/user.route")(app);
 
 server.on("listening", () => {
   const address = server.address();
